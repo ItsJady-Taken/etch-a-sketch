1469ec8875fbfee770d38a63babf627c64280f49
ItsJady-Taken <davidkhun2004@gmail.com>
2024-05-04 13:44:38 +0700

    Created a button to change frid squares
    Button allow user to change the squares size, 16x16 set as default

--- scripts.js
+++ scripts.js
@@ -1,16 +1,24 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-    createGrid()
+// after the DOM is loaded
+document.addEventListener('DOMContentLoaded', ()=> { 
+    // call function
+    createGrid(16);
+    const size_btn = document.querySelector('#size-btn');
+    size_btn.addEventListener('click', ()=> {
+        let size = changeSize()
+        createGrid(size)
+    })
+
 })
 
-function createGrid() {
-    let rows = 16;
-    let cols = 16;
+// Function that create a grid of divs and an argument of 'size' to change value of square perside
+function createGrid(size) {
     const grid_container = document.querySelector('#grid-container');
+    const gridSize = size * size; 
 
-    for(let i = 0; i < (rows * cols); i++) {
+    for(let i = 0; i < gridSize; i++) {
         const eachDiv = document.createElement('div');
-        eachDiv.style.width = `${600 / cols}px`;
-        eachDiv.style.height = `${600 / rows}px`;
+        eachDiv.style.width = `${600 / size}px`;
+        eachDiv.style.height = `${600 / size}px`;
         eachDiv.className = 'grid';
 
         eachDiv.addEventListener('mouseover', setColor);
@@ -21,5 +29,20 @@ function createGrid() {
 
 function setColor() {
     this.style.backgroundColor = 'black';
+}
+
+function changeSize() {
+    const num = prompt("Enter number between 1 and 100:");
+    if(num == ""){
+        alert("You didn't enter a number!")
+    }
+    else if(isNaN(num)){
+        alert(`${num} is not a number!`)
+           
+    }
+    else if(num <= 0 || num > 100) {
+        alert("Only numbers between 1 and 100!")
+    }
+   return createGrid(num);
 }
 
